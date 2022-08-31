ced2c68d6e11f917e86b24172a0058c2572b8f3d
Gustavo Lopes <glopoh@gmail.com>
2022-08-31 08:59:33 -0300

    Finalização e tratamento de exceção

--- MinhaCalculadoraDeSignos/Program.cs
+++ MinhaCalculadoraDeSignos/Program.cs
@@ -4,15 +4,33 @@
     {
         static void Main()
         {
-            Console.WriteLine("Que dia você nasceu? ");
-            int dia = int.Parse(Console.ReadLine());
-            Console.WriteLine("Que mes você nasceu? ");
-            int mes = int.Parse(Console.ReadLine());
+            int dia = 0;
+            int mes = 0;
+
+            try
+            {
+                Console.WriteLine("Que dia você nasceu? ");
+                dia = int.Parse(Console.ReadLine());
+                Console.WriteLine("Que mes você nasceu? ");
+                mes = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Não é permitido letras, digite apenas números!");
+                Main();
+            }
 
             InterpretadorSigno interpretador = new InterpretadorSigno();
             Signo signo = interpretador.Interpretar(dia, mes);
 
-            Console.WriteLine($"Seu signo é {signo.nome}, a característica do seu signo é {signo.caracteristicas}");
+            if (signo != null)
+            {
+                Console.WriteLine($"Seu signo é {signo.nome}, a característica do seu signo é {signo.caracteristicas}");
+            }
+            else
+            {
+                Console.WriteLine("Data incorreta! Tente novamente.");
+            }
         }
     }
 }
